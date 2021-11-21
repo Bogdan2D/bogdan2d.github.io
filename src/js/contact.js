5b65aded813b6b65c9dce237c6afbc0d19fff699
Diaconu Bogdan <diaconubogdan2016@gmail.com>
2021-11-21 17:57:12 +0200

    Initialized repo

---
+++ src/js/contact.js
@@ -0,0 +1,11 @@
+const contactIMG = document.getElementById('contactIMG');
+
+
+contactIMG.style.width = '225px;';
+contactIMG.style.height = '297px';
+contactIMG.style.borderRadius = '25px';
+contactIMG.style.display = 'flex';
+contactIMG.style.justifyContent = 'center';
+contactIMG.style.alignItems = 'center';
+
+
---
+++ src/js/games.js
@@ -0,0 +1,54 @@
+
+const games = [
+    {
+        "gameImage": "images/DashOVERLORD.png",
+        "gameTitle": "Dash OVERLORD",
+        "description": "Pass a few levels.<br>Sounds easy, right?"        
+    }
+];
+
+const baseContent = document.getElementById('baseContent')
+
+
+for(let i = 0; i < games.length; i++){
+
+    const img = document.createElement('img');
+    const gameTitle = document.createElement('h1');
+    const gameDesc = document.createElement('p');
+
+    const newDiv = document.createElement('div');
+
+    //img.alt = gameImage;
+    img.src = games[i].gameImage;
+    gameTitle.innerHTML = games[i].gameTitle;
+    gameTitle.id = "gameTitle"
+    gameTitle.style.color = 'white';
+
+    gameDesc.innerHTML = games[i].description;
+    gameDesc.id = "gameDesc";
+    gameDesc.style.color = 'white';
+
+    img.onload = function(){
+        img.width = 330;
+        img.height = 262;
+        //img.style.display = 'flex';
+        //img.style.justifyContent = 'center';
+        img.style.marginTop = '1rem';
+        img.style.borderRadius = '25px';
+        img.style.border = '5px solid #555';
+    }
+    
+    img.onclick = function(){
+        alert("You are about the leave this page.");
+        switch(gameTitle.innerHTML){
+            case 'Dash OVERLORD':
+                window.open('https://www.newgrounds.com/portal/view/819901', '_self');
+        }
+    }
+
+    newDiv.appendChild(img);
+    newDiv.appendChild(gameTitle);
+    newDiv.appendChild(gameDesc);
+
+    baseContent.appendChild(newDiv);
+}
