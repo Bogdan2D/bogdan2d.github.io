0a392434406684bbeb1e477ebf85b0443f41a7b4
Diaconu Bogdan <diaconubogdan2016@gmail.com>
2021-11-24 12:09:11 +0200

    Made the navBar a JS component.

---
+++ src/js/navBar.js
@@ -0,0 +1,40 @@
+//-----[HEADER]-----\\
+const myName = document.createElement('h1');
+myName.id = "myName";
+myName.innerHTML = "Bogdan2D";
+
+const subText = document.createElement('p') //i should make the text random :>
+subText.id = "subText";
+subText.innerHTML = "I make stuff"
+//--------------------\\
+
+const options = ['Home', 'Games', 'About', 'Contact'];
+const optionsRedirects = ['index', 'games', 'about', 'contact'];
+
+const navBar = document.createElement('div');
+navBar.id = "navBar";
+
+
+for(let i = 0; i < options.length; i++){
+    const newOptionSpan = document.createElement('span');
+    const newOptionText = document.createElement('p');
+    const newOptionRedirect = document.createElement('a');
+
+    newOptionRedirect.innerHTML = options[i];
+    newOptionRedirect.href = optionsRedirects[i] + ".html";
+    
+    newOptionSpan.appendChild(newOptionText);
+    newOptionText.appendChild(newOptionRedirect);
+    
+
+    if(i != 0){
+        newOptionText.style.marginLeft = '1rem';
+    }
+
+    navBar.appendChild(newOptionSpan);
+}
+
+document.body.appendChild(myName);
+document.body.appendChild(subText);
+document.body.appendChild(navBar);
+document.body.appendChild(document.createElement('hr'));
